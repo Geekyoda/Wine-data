803476b59048d79e805f06f20e4edaa04b47e34d
Geekyoda <1104satyamkr@gmail.com>
2023-10-29 00:12:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 
 import React from 'react';
-import FlavanoidsStatistics from './componenets/FlavanoidsStatistics'; // Import FlavanoidsStatistics component
-import GammaStatistics from './componenets/GammaStatistics'; // Import GammaStatistics component
+import FlavanoidsStatistics from './componenets/FlavanoidsStatistics'; 
+import GammaStatistics from './componenets/GammaStatistics'; 
 
 function App() {
   return (
     <div>
       <h1>Flavanoids Statistics</h1>
-      <FlavanoidsStatistics /> {/* Render FlavanoidsStatistics component */}
+      <FlavanoidsStatistics /> 
       <h1>Gamma Statistics</h1>
-      <GammaStatistics /> {/* Render GammaStatistics component */}
+      <GammaStatistics /> 
     </div>
   );
 }
